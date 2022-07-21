045bc9d663c90ebcbb731906f4c927c4e55da9af
Justin Goncalves <jvgoncalves85@gmail.com>
2022-07-21 16:59:34 -0400

    created reply model

--- backend/reply/models.py
+++ backend/reply/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from authentication.models import User
+from comment.models import Comment
 # Create your models here.
+
+class Reply(models.Model):
+    # USER THAT IS MAKING THE REPLY
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # COMMENT WE ARE REPLING TOO 
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    text = models.CharField(max_length=300)
