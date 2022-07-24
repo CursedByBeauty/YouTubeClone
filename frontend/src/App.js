5e5424bab9f53c548991d42734cafcec77440776
Justin Goncalves <jvgoncalves85@gmail.com>
2022-07-24 18:31:59 -0400

    Added videos to homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,24 +11,31 @@ import RegisterPage from "./pages/RegisterPage/RegisterPage";
 // Component Imports
 import Navbar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { KEY } from "./localKey";
 
-// Util Imports
-import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
   const [user, token] = useAuth();
+  const [videos, setVideos] = useState([]);
+
+  useEffect(()=>{
+    getAllVideos()
+  },[])
+  async function getAllVideos(){
+    try {
+      let request = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=tloufactions&type=video&part=snippet&key=${KEY}&maxResults=8`)
+      setVideos(request.data.items)
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route
-          path="/"
-          element={
-            <PrivateRoute>
-              <HomePage />
-            </PrivateRoute>
-          }
-        />
+        <Route path="/" element={<HomePage videos={videos} user={user} token={token} />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,7 +1,33 @@
 import React from "react";
-const HomePage = () => {
+const HomePage = (props) => {
   return (
     <div className="container">
+      {props.videos.map((video, index) => {
+        if (video.snippet) {
+          return (
+            <span key={index * 2}>
+              <div>
+                <input
+                  type="image"
+                  src={video.snippet.thumbnails.default.url}
+                  alt="Video Thumbnail"
+                />
+              </div>
+              <div>
+                {" "}
+                <h3>{video.snippet.title}</h3>
+              </div>
+            </span>
+          );
+        } else
+          return (
+            <span>
+              <div>
+                <h6>Loading...</h6>
+              </div>
+            </span>
+          );
+      })}
     </div>
   );
 };
