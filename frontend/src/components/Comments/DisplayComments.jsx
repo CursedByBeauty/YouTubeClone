8312fdd0e845a785f0a8ca7839976f0f60da5d3a
Justin Goncalves <jvgoncalves85@gmail.com>
2022-07-30 16:40:44 -0400

    Added functionality to adding a reply

--- frontend/src/components/Comments/DisplayComments.jsx
+++ frontend/src/components/Comments/DisplayComments.jsx
@@ -1,26 +1,43 @@
-
 import React, { useEffect } from "react";
 import useAuth from "../../hooks/useAuth";
 import DisplayReplies from "../Replies/DisplayReplies";
 import ReplyForm from "../Replies/ReplyForm";
+import axios from "axios";
 const DisplayComments = (props) => {
-    const [user, token] = useAuth()
-  useEffect(()=>{
-    props.getAllComments()
-  },[])
-  return <div>
-{props.comments.filter((comment)=> comment.video_id === props.videoId).map((item)=>{
-    return (
-        <div key={item.id*2}>
-        <h4>{item.text}</h4>
-        <button>Likes: {item.likes}</button>
-        <button>DisLikes: {item.dislikes}</button>
-        <DisplayReplies token={token} commentId={item.id}/>
-        <ReplyForm token={token}/>
-        </div>
-    )
-})}
-  </div>;
+  const [user, token] = useAuth();
+  useEffect(() => {
+    props.getAllComments();
+  }, []);
+
+  async function createReply(body) {
+    try {
+      await axios.post("http://127.0.0.1:8000/reply/", body, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      });
+      props.getAllComments()
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+  return (
+    <div>
+      {props.comments
+        .filter((comment) => comment.video_id === props.videoId)
+        .map((item) => {
+          return (
+            <div key={item.id * 2}>
+              <h4>{item.text}</h4>
+              <button>Likes: {item.likes}</button>
+              <button>DisLikes: {item.dislikes}</button>
+              <DisplayReplies token={token} commentId={item.id} />
+              <ReplyForm createReply={createReply} commentId={item.id} user={user} />
+            </div>
+          );
+        })}
+    </div>
+  );
 };
 
 export default DisplayComments;
--- frontend/src/components/Replies/ReplyForm.jsx
+++ frontend/src/components/Replies/ReplyForm.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 const ReplyForm = (props) => {
+  const [replyText, setReplyText] = useState('');
+
+  function handleAdd(description) {
+    let newReply = {
+      user_id: props.user.id,
+      comment_id: props.commentId,
+      text: description,
+    };
+    props.createReply(newReply);
+    setReplyText('')
+  }
   return (
     <div>
-      <input type="text" />
-      <button>ADD</button>
+      <input
+        value={replyText}
+        type="text"
+        onChange={(event) => setReplyText(event.target.value)}
+      />
+      <button onClick={() => handleAdd(replyText)}>ADD</button>
     </div>
   );
 };
