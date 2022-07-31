5f36baba893bd9f518eae044edcc903bbdad34d1
Justin Goncalves <jvgoncalves85@gmail.com>
2022-07-31 18:05:27 -0400

    Adjusted card display video css

--- frontend/src/components/DisplayVideos/DisplayVideos.jsx
+++ frontend/src/components/DisplayVideos/DisplayVideos.jsx
@@ -1,30 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../App.css";
 const DisplayVideos = (props) => {
   return (
-    <div>
+    <div className="grid-box">
       {props.videos.map((video, index) => {
         if (video.snippet) {
           return (
-            <span key={index * 3}>
+            <div className="card" >
+            <div key={index * 3} className="grid-item">
               <Link to={`/${video.id.videoId}/`}>
                 <div>
                   <input
+                  className="card-img-top"
                     type="image"
                     src={video.snippet.thumbnails.medium.url}
                     alt="Video Thumbnail"
                   />
                 </div>
-                <div>
+                <div className="card-body">
                   {" "}
-                  <h4>{video.snippet.title}</h4>
+                  <h4 className="card-text">{video.snippet.title}</h4>
                 </div>
               </Link>
-            </span>
+            </div>
+            </div>
           );
         } else
           return (
-            <span key={index*3}>
+            <span key={index * 3} className="grid-item">
               <div>
                 <h6>Loading...</h6>
               </div>
